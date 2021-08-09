0534d9d5c10cc1f7374ab6f0df168d90eab74d63
Vladislav Kuznetsov <starforce882@gmail.com>
2021-08-09 14:23:20 +0300

    Logging implementation

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 # оставь надежду всяк сюда входящий
+# todo: логирование
 
 import os
+import logging
 import telebot
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logging.basicConfig(filename='log.txt', level=logging.DEBUG,
+                    format='%(asctime)s : %(levelname)s ::: %(message)s')
 
 KEY = os.getenv('API_KEY')
 bot = telebot.TeleBot(KEY)
@@ -13,14 +18,16 @@ bot = telebot.TeleBot(KEY)
 
 @bot.message_handler(commands=['hello'])
 def greet(message):
-    bot.reply_to(message, f'Привет @{message.from_user.username}! Как дела?')
+    bot.reply_to(message, f'Привет, @{message.from_user.username}! Как дела?')
+    logging.info(f'{message.from_user.username} typed /hello')
 
 
 @bot.message_handler(commands=['commands'])
-def help(message):
+def helpme(message):
     bot.send_message(message.chat.id, '`/commands - показывает весь список доступных команд\n`'
                                       '`/info - обо мне`\n'
                                       '`/hello - здоровается с написавшим`', parse_mode="MARKDOWN")
+    logging.info(f'{message.from_user.username} typed /commands')
 
 
 @bot.message_handler(commands=['info'])
@@ -29,6 +36,7 @@ def about(message):
                                       'Я учу русский язык уже пять лет. Мои знания не очень большие, но '
                                       'у меня много русских и украинских друзей. Я хочу практиковаться и '
                                       'учиться больше. Давайте дружить!')
+    logging.info(f'{message.from_user.username} typed /info')
 
 
 bot.polling()
